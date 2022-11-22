249e02098e38500175dfa2ded916900bfe417ded
Lunarmagpie <Bambolambo0@gmail.com>
2022-11-21 20:49:49 -0500

    Add mypyc build  (#1)

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
+from setuptools import setup
 from mypyc.build import mypycify
+
 
 sources = ("__init__.py", "lib.py", "rule.py", "common.py")
 ext_modules = mypycify([f"src/crossandra/{i}" for i in sources])
 
-
-def build(setup_kwargs):
-    setup_kwargs["ext_modules"] = ext_modules
-    setup_kwargs["packages"] = ["crossandra"]
+setup(
+    name="crossandra",
+    packages=["src/crossandra"],
+    ext_modules=ext_modules,
+)
