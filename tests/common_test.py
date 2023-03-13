db23f66ece26827af714237a076a0035671c6a9b
Qexat <43090614+qexat@users.noreply.github.com>
2023-03-13 11:28:57 +0100

    Added tests

--- tests/common_test.py
+++ tests/common_test.py
@@ -110,6 +110,19 @@ def test_INT(string, result):
 @pytest.mark.parametrize(
     ["string", "result"],
     [
+        ("+69", (69, 3)),  # POSITIVE INT
+        ("-69", (-69, 3)),  # NEGATIVE INT
+        ("69", NOT_APPLIED),  # NOT SIGNED
+        ("", NOT_APPLIED),  # EMPTY STRING
+    ],
+)
+def test_SIGNED_INT(string, result):
+    assert SIGNED_INT.apply(string) == result
+
+
+@pytest.mark.parametrize(
+    ["string", "result"],
+    [
         ("3.14", (3.14, 4)),  # NORMAL DECIMAL  [INCORRECTLY FAILING]
         ("3.0", (3.0, 3)),  # POST-COMMA TRAILING ZERO
         ("69.42", (69.42, 5)),  # MULTI-DIGIT INT BASE DECIMAL  [INCORRECTLY FAILING]
@@ -121,3 +134,51 @@ def test_INT(string, result):
 )
 def test_DECIMAL(string, result):
     assert DECIMAL.apply(string) == result
+
+
+@pytest.mark.parametrize(
+    ["string", "result"],
+    [
+        ("3", (3, 1)),  # NORMAL HEX: DIGIT
+        ("D", (13, 1)),  # NORMAL HEX: UPPERCASE LETTER
+        ("a", (10, 1)),  # NORMAL HEX: LOWERCASE LETTER
+        ("g", NOT_APPLIED),  # INVALID HEX VALUE
+        ("", NOT_APPLIED),  # EMPTY STRING
+    ],
+)
+def test_HEXDIGIT(string, result):
+    assert HEXDIGIT.apply(string) == result
+
+
+@pytest.mark.parametrize(
+    ["string", "result"],
+    [
+        ("word", ("word", 4)),  # ONE WORD
+        ("two_words", ("two_words", 9)),  # TWO WORDS, UNDERSCORE SEP
+        ("_word", ("_word", 5)),  # 'PRIVATE' ONE WORD
+        ("_two_words", ("_two_words", 10)),  # 'PRIVATE' TWO WORDS, UNDERSCORE SEP
+        ("0word", NOT_APPLIED),  # LEADING DIGIT
+        ("word0", ("word0", 5)),  # TRAILING DIGIT
+        ("_0word", ("_0word", 6)),  # 'PRIVATE' LEADING DIGIT
+        ("_word0", ("_word0", 6)),  # 'PRIVATE' TRAILING DIGIT
+        ("0", NOT_APPLIED),  # SINGLE DIGIT
+        ("69420", NOT_APPLIED),  # SEVERAL DIGITS
+        ("", NOT_APPLIED),  # EMPTY STRING
+    ],
+)
+def test_C_NAME(string, result):
+    assert C_NAME.apply(string) == result
+
+
+@pytest.mark.parametrize(
+    ["string", "result"],
+    [
+        ("\n", ("\n", 1)),  # LF NEWLINE
+        ("\r\n", NOT_APPLIED),  # CRLF NEWLINE
+        ("\r", NOT_APPLIED),  # CR NEWLINE
+        ("\\n", NOT_APPLIED),  # ESCAPED LF NEWLINE
+        ("", NOT_APPLIED),  # EMPTY STRING
+    ],
+)
+def test_NEWLINE(string, result):
+    assert NEWLINE.apply(string) == result
